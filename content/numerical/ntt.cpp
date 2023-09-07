544053e18f8b4dc8c81bcb44128dc21b32fcffc6
Boboge <50315826+avgstuBoboge@users.noreply.github.com>
2023-09-07 22:52:17 +0200

    change the statement

--- content/numerical/ntt.cpp
+++ content/numerical/ntt.cpp
@@ -3,16 +3,12 @@
  * Date: 23-03-29
  * Description: Number Theoretic Transform. 
  *  class $T$ should have static function $getMod()$ to provide the $mod$. We usually just use $modnum$ as the template parameter.
- *  To keep the code short we just set the primitive root as $3$. However, it might be wrong when $mod \ne 998244353$. Here are some commonly used $mod$s and the corresponding primitive root.\\
  *  $g \to  mod~(\max \log(n))$:\\
  *  3 $\to$ 104857601 (22), 167772161 (25), 469762049 (26), 998244353 (23), 1004535809 (21);\\
  *  10 $\to$ 786433 (18);\\
  *  31 $\to$ 2013265921 (27).
- * Usage: const int mod = 998244353;
- *  using Mint = Z<mod>; // Z is modnum struct.
- *  ...
- *  FFT<Mint> ntt(3); // use 3 as primitive root.
- *  vector<Mint> as = ntt.conv(vector<Mint>{1, 2, 3}, vector<Mint>{2, 3, 4});
+ * Usage:
+ *  vector<Z> as = FFT.conv(vector<Z>{1, 2, 3}, vector<Z>{2, 3, 4});
  * Time: O(N \log N).
  * Status: tested on https://ac.nowcoder.com/acm/contest/33186/H, https://judge.yosupo.jp/problem/kth_term_of_linearly_recurrent_sequence.
  */
