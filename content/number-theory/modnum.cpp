30f70b7f9a84d6ce5fb8cc93d661b5e6c31879aa
Boboge <avgstuboboge@gmail.com>
2024-01-04 16:33:26 +0100

    fit code lib for next competition

--- content/number-theory/modnum.cpp
+++ content/number-theory/modnum.cpp
@@ -5,8 +5,8 @@
    1. You do not need to keep writing $\%\, mod$;
    2. It is good to use this struct when doing Gaussian Elimination / Fast Walsh-Hadamard Transform;
    3. Sometimes the input number is greater than $mod$ and this code handles it.
-  Do not write things like $Mint\{1 / 3\}.pow(10)$ since $1 / 3$ simply equals $0$.
-  Do not write things like $Mint\{a * b\}$ where $a$ and $b$ are int since you might first have integer overflow.
+  Do not write things like $mint\{1 / 3\}.pow(10)$ since $1 / 3$ simply equals $0$.
+  Do not write things like $mint\{a * b\}$ where $a$ and $b$ are int since you might first have integer overflow.
  * Usage: ...
  * Status: tested on https://ac.nowcoder.com/acm/contest/33191/F.
  */
@@ -22,12 +22,12 @@ T power(T a, int b) {
 }
 
 template<int P>
-struct MInt {
+struct mint {
     int x;
 
-    constexpr MInt() : x{} {}
+    constexpr mint() : x{} {}
 
-    constexpr MInt(ll x) : x{norm(x % getMod())} {}
+    constexpr mint(ll x) : x{norm(x % getMod())} {}
 
     static int Mod;
 
@@ -61,81 +61,62 @@ struct MInt {
         return x;
     }
 
-    constexpr MInt operator-() const {
-        MInt res;
+    constexpr mint operator-() const {
+        mint res;
         res.x = norm(getMod() - x);
         return res;
     }
 
-    constexpr MInt inv() const {
+    constexpr mint inv() const {
         assert(x != 0);
         return power(*this, getMod() - 2);
     }
 
-    constexpr MInt &operator*=(MInt rhs) &{
+    constexpr mint &operator*=(mint rhs) &{
         x = 1LL * x * rhs.x % getMod();
         return *this;
     }
 
-    constexpr MInt &operator+=(MInt rhs) &{
+    constexpr mint &operator+=(mint rhs) &{
         x = norm(x + rhs.x);
         return *this;
     }
 
-    constexpr MInt &operator-=(MInt rhs) &{
+    constexpr mint &operator-=(mint rhs) &{
         x = norm(x - rhs.x);
         return *this;
     }
 
-    constexpr MInt &operator/=(MInt rhs) &{
+    constexpr mint &operator/=(mint rhs) &{
         return *this *= rhs.inv();
     }
 
-    friend constexpr MInt operator*(MInt lhs, MInt rhs) {
-        MInt res = lhs;
+    friend constexpr mint operator*(mint lhs, mint rhs) {
+        mint res = lhs;
         res *= rhs;
         return res;
     }
 
-    friend constexpr MInt operator+(MInt lhs, MInt rhs) {
-        MInt res = lhs;
+    friend constexpr mint operator+(mint lhs, mint rhs) {
+        mint res = lhs;
         res += rhs;
         return res;
     }
 
-    friend constexpr MInt operator-(MInt lhs, MInt rhs) {
-        MInt res = lhs;
+    friend constexpr mint operator-(mint lhs, mint rhs) {
+        mint res = lhs;
         res -= rhs;
         return res;
     }
 
-    friend constexpr MInt operator/(MInt lhs, MInt rhs) {
-        MInt res = lhs;
+    friend constexpr mint operator/(mint lhs, mint rhs) {
+        mint res = lhs;
         res /= rhs;
         return res;
-    }
-
-    friend constexpr std::istream &operator>>(std::istream &is, MInt &a) {
-        ll v;
-        is >> v;
-        a = MInt(v);
-        return is;
-    }
-
-    friend constexpr std::ostream &operator<<(std::ostream &os, const MInt &a) {
-        return os << a.val();
-    }
-
-    friend constexpr bool operator==(MInt lhs, MInt rhs) {
-        return lhs.val() == rhs.val();
-    }
-
-    friend constexpr bool operator!=(MInt lhs, MInt rhs) {
-        return lhs.val() != rhs.val();
     }
 };
 
 template<>
-int MInt<0>::Mod = 998244353;
+int mint<0>::Mod = 998244353;
 
-using Z = MInt<0>;
+using Z = mint<0>;
