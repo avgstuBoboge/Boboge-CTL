552f9bb5134781f75d00886d9ce4cf2ad7be2179
Botu Lyu <avgstuBoboge@gmail.com>
2024-03-10 00:47:39 +0100

    M  content/datastruct/fenwick.cpp

--- content/datastruct/fenwick.cpp
+++ content/datastruct/fenwick.cpp
@@ -14,21 +14,31 @@ struct Fenwick {
     int n;
     vector<T> t;
 
-    Fenwick(int n) : n(n + 1), t(n + 2) {};
+    Fenwick(int n) : n(n), t(n + 1) {};
 
     void update(int x, T v) {
-        x++;
-        for (; x <= n; x += x & -x) t[x] += v;    
+        for (++x; x <= n; x += x & -x) t[x] += v;
     }
 
     T query(int x) {
-        x++;
         T ret{};
-        for (; x; x -= x & -x) ret += t[x];
+        for (++x; x; x -= x & -x) ret += t[x];
         return ret;
     }
 
     T query(int l, int r) {
         return query(r) - query(l - 1);
+    }
+
+    // Assuming prefix sums are non-decreasing, finds last i s.t. get(i) <= v
+    int search(T v) {
+        int res = 0;
+        for (int h = 1 << 30; h; h >>= 1) {
+            if ((res | h) < t.size() && t[res | h] <= v) {
+                res |= h;
+                v -= t[res];
+            }
+        }
+        return res - 1;
     }
 };
