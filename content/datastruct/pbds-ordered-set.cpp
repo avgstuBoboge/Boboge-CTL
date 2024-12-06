f939683ca4190185013e9cb70ef5033107882c73
root <root@DESKTOP-5KFT7MF>
2024-12-06 14:37:10 +0100

    M  content/datastruct/pbds-ordered-set.cpp

--- content/datastruct/pbds-ordered-set.cpp
+++ content/datastruct/pbds-ordered-set.cpp
@@ -5,3 +5,4 @@ template<class T>
 using Oset = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; // if null_type does not work, then use null_mapped_type instead.
 // order_of_key(x) returns the number of elements which are smaller than x. (quite like lower_bound.)
 // find_by_order(x) returns the x-th smallest element.
+// Change to less_equal<T> if you want multiset. Write ms.erase(prev(ms.lower_bound(key))) to erase an element. Be careful to use this.
