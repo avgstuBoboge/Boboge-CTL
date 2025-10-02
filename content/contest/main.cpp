54a296692744af1017731caa26546f343d3b0a6d
avgstuBoboge <50315826+avgstuBoboge@users.noreply.github.com>
2025-10-03 01:01:22 +0200

    Add macros for iteration and container size
    Added macro definitions for loop iterations and container utilities.

--- content/contest/main.cpp
+++ content/contest/main.cpp
@@ -8,7 +8,13 @@
 #define debug(...) (void)(0721)
 #endif
 
+#define rep(i, a, n) for (auto i = a; i <= (n); ++i)
+#define revrep(i, a, n) for (auto i = n; i >= (a); --i)
+#define all(a) a.begin(), a.end()
+#define sz(a) (int)(a).size()
 using namespace std;
+using pii = pair<int, int>;
+using vi = vector<int>;
 using i64 = long long;
 using u64 = unsigned long long;
 using i128 = __int128;
